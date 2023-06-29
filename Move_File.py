23be9f41d4ed247d46652408af8ec06f7b8c4a8d
Anoncoder9999 <137862671+Anoncoder9999@users.noreply.github.com>
2023-06-29 12:42:31 +0300

    Add files via upload

---
+++ Move_File.py
@@ -0,0 +1,10 @@
+ import os
+import shutil
+
+from_dir = "\Document__Files"
+to_dir = "\Documents\Downloads"
+list_of_files = os.listdir(from_dir)
+print (list_of_files) 
+
+os.path.splitext()
+print()
